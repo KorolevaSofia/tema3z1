62b245a944d177dc7f924b66fe30c7c8b9d39df4
Sonya K <magvanss@yandex.ru>
2023-12-11 20:52:20 +0300

    second commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13696;
+        int rubles = 20;
+        int miles = price / rubles;
+        System.out.print("Количество начисленных миль =");
+        System.out.print(miles);
+    }
+}
